7554beac166ee54a01caaea2e0e56f73d17ead7d
RiccardoAccatino <20054233@studenti.uniupo.it>
2025-11-18 16:47:31 +0100

    Esercizio4 corretto

--- PrimiEsercizi/src/primiesercizi/Esercizio4.java
+++ PrimiEsercizi/src/primiesercizi/Esercizio4.java
@@ -6,16 +6,18 @@ public class Esercizio4 {
 	{
 		Input input= new Input();
 		String s=input.readString("Inserisci la Stringa=");
-		String s1=null; //Tolgo i caratteri di indice dispari
-		String s2=null; //Faccio il reverse della stringa
+		String s1=""; //Tolgo i caratteri di indice dispari
+		String s2=""; //Faccio il reverse della stringa
 		for(int i=0; i<s.length(); i++)
 		{
+			char c = s.charAt(i);
 			if(i%2==0)
 			{
-				s1.concat(s.charAt(i););
-				
+				s1=s1+c;
 			}
-			//s2= s2+s.charAt(s.length()-i);
+			
+			s2= c + s2;
+			
 		}
 		System.out.println("s1="+s1);
 		System.out.println("s2="+s2);
